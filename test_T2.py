4f629526e84b484bec28eb9758c4065251cc75d4
Ilya <topol123@mail.ru>
2025-11-19 15:55:06 +0300

    19 11 25 Создал файл test_T2.py для экспериментов

---
+++ test_T2.py
@@ -0,0 +1,17 @@
+import requests
+
+
+import response
+
+
+
+def rest_main ():
+    response = requests
+
+if __name__ == '__main__':
+        print_hi('PyCharm')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
